3175b94bc21aba086259d761525dd855869e5ae8
Geoffrey Eckert <eckertg1@yahoo.com>
2018-07-27 04:53:00 +0000

    assignment 8 complete

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,31 @@
+class AdvertisementsController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisements = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisements = Advertisement.new
+  end
+
+  def create
+    @advertisements = Advertisement.new
+    @advertisements.title = params[:advertisement][:title]
+    @advertisements.copy = params[:advertisement][:copy]
+    @advertisements.price = params[:advertisement][:price]
+    
+    if @advertisements.save
+      flash[:notice] = "Advertisement was saved"
+      redirect_to @advertisements
+    else
+      flash.now[:alert] = "There was an error saving the advertisement. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+  end
+end
